20471bfb17b5f09586781923d333b9ef293532b2
Shiz <mark@xn--hwg34fba.ws>
2013-11-03 03:09:02 +0100

    [modules/waitopiggu] Add waitopiggu module.
    This module allows you to configure Michiru to automatically ban a joining user
    that is present on (one or more) configurable channels. This allows you to
    efficiently deal with 'raids' or generally unpleasant source atmospheres.
    This module needs to be explicitly enabled, as it is not by default.
    
    Configuration values introduced:
    - waitopiggu_whitelist: A nickname whitelist of people who will not be affected
       by this module.
    - waitopiggu_channels: A list of 'bad' source channels, upon which a joining
       user will be banned if one or more occurs in their WHOIS list.
    - waitopiggu_ban_method: Michiru can ban users using two methods: a direct
       host (`direct`) mask ban (which will use `*!*@host`), or a 'ban proxy'
       (`proxy`), causing Michiru to send a certain formatted private message to
       another user who will supposedly ban the offending user, or just log their
       existence, or do whatever they feel is appropriate.
    - waitopiggu_ban_proxy: The nickname of the user that will be private messaged
       when using the `proxy` ban method.
    - waitopiggu_ban_proxy_format: The message format that shall be used when
       private messaging the ban proxy when using the `proxy` ban method. This
       string shall be formatted according to Python's advanced string formatting
       (the `.format` method), and be filled in with the `{nick}`, `{server}`,
       `{channel}` and `{message}` variables. See below for what the `message`
       variable means.
    - waitopiggu_ban_message: The ban message. When using the `direct` ban method
       this will be the kick message, or if using the `proxy` method it will be
       available in the formatted message string as `{message}`.

---
+++ michiru/modules/waitopiggu.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import config
+import db
+import personalities
+from modules import hook
+_ = personalities.localize
+
+config.ensure('waitopiggu_channels', [])
+config.ensure('waitopiggu_whitelist', [])
+config.ensure('waitopiggu_ban_method', 'direct')
+config.ensure('waitopiggu_ban_message', _('Your origins are not conducive to the desired environment.'))
+config.ensure('waitopiggu_ban_proxy', None)
+config.ensure('waitopiggu_ban_proxy_format', None)
+
+__name__ = 'waitopiggu'
+__author__ = 'Shiz'
+__license__ = 'WTFPL'
+
+@hook('irc.join')
+def join(bot, server, channel, who):
+    if who[0] in config.list('waitopiggu_whitelist', server, channel):
+        return
+
+    # Get channel list and strip status info.
+    info = bot.whois(who[0])
+    channels = [ chan.lstrip('!~@%+') for chan in info['CHANNELS'] ]
+    blacklist = config.list('waitopiggu_channels', server, channel)
+
+    # User is in a bad channel?
+    if set(channels).intersection(set(blacklist)):
+        method = config.get('waitopiggu_ban_method', server, channel)
+        message = config.get('waitopiggu_ban_message', server, channel)
+
+        # Direct kick?
+        if method == 'direct':
+            bot.cmode(channel, '+b *!*@{mask}'.format(mask=who[2]))
+            bot.kick(channel, who[0], message)
+        # Or kick-by-proxy?
+        elif method == 'proxy':
+            proxy = config.get('waitopiggu_ban_proxy', server, channel)
+            msg = config.get('waitopiggu_ban_proxy_format', server, channel)
+            bot.privmsg(proxy, msg.format(nick=who[0], server=server, channel=channel, message=message))
+
+def load():
+    return False
+
+def unload():
+    pass
